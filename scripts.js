c662e723bc04556e19c40299d65d75cb76f4033a
Kennyx85110 <114527645+Kennyx85110@users.noreply.github.com>
2022-12-02 10:23:49 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,61 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+let sliders = document.querySelectorAll('.slider');
+
+sliders.forEach(function (slider) {
+    let sliderRange = slider.querySelector('.slider__range');
+    let sliderBefore = slider.querySelector('.slider__before');
+    let sliderSeparator = slider.querySelector('.slider__separator');
+
+    function updateSliderPosition() {
+        sliderBefore.style = `width:${sliderRange.value}%`;
+        sliderSeparator.style = `left:${sliderRange.value}%`;
+    }
+
+    sliderRange.addEventListener('input', updateSliderPosition);
+});
+
